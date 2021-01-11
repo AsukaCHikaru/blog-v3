0d2d13b09c23d45ab29541195bbceda6dfbad72a
asukachikaru <asukachikaru@gmail.com>
2021-01-11 15:31:45 +0900

    add log to webpack.server.config.ts

--- webpack.server.config.ts
+++ webpack.server.config.ts
@@ -2,43 +2,47 @@ import * as webpack from "webpack";
 import { resolve } from "path";
 import * as nodeExternals from "webpack-node-externals";
 
-const config: webpack.Configuration = {
-  mode: "development",
-  entry: "./src/server/index.ts",
-  context: resolve(__dirname),
-  output: {
-    filename: "server.bundle.js",
-    path: resolve(__dirname, "dist", "server"),
-  },
-  target: "node",
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: ["babel-loader", "ts-loader"],
-      },
+const config = (): webpack.Configuration => {
+  console.log(process.env);
+
+  return {
+    mode: "development",
+    entry: "./src/server/index.ts",
+    context: resolve(__dirname),
+    output: {
+      filename: "server.bundle.js",
+      path: resolve(__dirname, "dist", "server"),
+    },
+    target: "node",
+    node: {
+      __dirname: false,
+      __filename: false,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: ["babel-loader", "ts-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".tsx"],
+    },
+    externals: [nodeExternals()],
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": {
+          CONTENTFUL_TOKEN: JSON.stringify(process.env.CONTENTFUL_TOKEN || ""),
+          CONTENTFUL_SPACE_ID: JSON.stringify(
+            process.env.CONTENTFUL_SPACE_ID || ""
+          ),
+          PORT: JSON.stringify(process.env.PORT) || 8080,
+        },
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".tsx"],
-  },
-  externals: [nodeExternals()],
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        CONTENTFUL_TOKEN: JSON.stringify(process.env.CONTENTFUL_TOKEN || ""),
-        CONTENTFUL_SPACE_ID: JSON.stringify(
-          process.env.CONTENTFUL_SPACE_ID || ""
-        ),
-        PORT: JSON.stringify(process.env.PORT) || 3000,
-      },
-    }),
-  ],
+  };
 };
 
 export default config;
